30398171ef999e8179918070587cfa2c74e4ece3
bachdev <76158313+mohamedelbachir@users.noreply.github.com>
2025-04-22 21:56:22 +0000

    fix some bug

--- lib/pdf-to-img.ts
+++ lib/pdf-to-img.ts
@@ -4,22 +4,24 @@ import { PDFPageProxy } from "pdfjs-dist/types/src/display/api";
 pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
 
 const loadPdf = async (file: File): Promise<PDFPageProxy[]> => {
-  let uri: string
-  uri = URL.createObjectURL(file)
+  const uri = URL.createObjectURL(file);
   const pdf = await pdfjsLib.getDocument({ url: uri }).promise;
 
-  const pages: pdfjsLib.PDFPageProxy[] = [];
+  const pages: PDFPageProxy[] = [];
   for (let i = 1; i <= pdf.numPages; i++) {
     const page = await pdf.getPage(i);
     pages.push(page);
   }
   return pages;
 };
-
-const renderPageToImage = async (page: pdfjsLib.PDFPageProxy, scale: number = 3): Promise<string> => {
+// pdf-to-img.ts
+const renderPageToImage = async (
+  page: pdfjsLib.PDFPageProxy,
+  scale: number = 3
+): Promise<string> => {
   const viewport = page.getViewport({ scale });
   const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d", { willReadFrequently: true });
+  const context = canvas.getContext("2d");
 
   if (!canvas || !context) {
     throw new Error("Canvas or context is null.");
@@ -43,11 +45,9 @@ const renderPageToImage = async (page: pdfjsLib.PDFPageProxy, scale: number = 3)
 
   await renderTask.promise;
 
-  // Reduce image quality to 0.5
-  const image = canvas.toDataURL("image/jpeg", 0.5);
-
-  return image;
+  return canvas.toDataURL();
 };
+
 
 export const pdfToImg = async (file: File): Promise<string[]> => {
   try {
--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, {}) => {
-        config.resolve.alias.canvas = false
-        return config
-    }
+    future: { webpack5: true },
+  webpack: (config, {}) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 }
 
 module.exports = nextConfig
